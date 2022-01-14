542b564eddc770c49a48a7345816011751ba24eb
anirudhkmitl <makhana.anirudh@gmail.com>
2022-01-14 11:40:31 +0700

    [ADDED]: Networks and tests

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,9 +1,22 @@
-from brownie import FundMe
+from brownie import FundMe, MockV3Aggregator, network, config
+from scripts.helpful_scripts import getAccount
 
 
 def deployFundMe():
     # First we need to get account
     account = getAccount()
+    # pass the price feed into the deploy as we have now added the constructor.
+    # if we are on persistent network (rinekby) use the associated address otherwise deploy mocks.
+    if network.show_active() != "development":
+        priceFeedAddress = config["networks"][network.show_active()][
+            "eth_usd_price_feed"
+        ]
+    fundMe = FundMe.deploy(
+        priceFeedAddress,
+        {"from": account},
+        publish_source=True,
+    )
+    print(f"Contract deployed to {fundMe.address}")
 
 
 def main():
